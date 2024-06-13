fc809e61d0cd3318ba48cb5171626e184d849369
kangkang088 <1808131844@qq.com>
2024-06-13 15:43:52 +0800

    no message

--- 5C#高级语法/01正则表达式/Program.cs
+++ 5C#高级语法/01正则表达式/Program.cs
@@ -1,12 +1,9 @@
-﻿
-namespace _01正则表达式
+﻿namespace _01正则表达式
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            ArgumentNullException.ThrowIfNull(args);
-
             Console.WriteLine("Hello, World!");
         }
     }
--- 5C#高级语法/01正则表达式/obj/Debug/net8.0/01正则表达式.AssemblyInfo.cs
+++ 5C#高级语法/01正则表达式/obj/Debug/net8.0/01正则表达式.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("01正则表达式")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af29b4989e5b2030303fad227fba99b33910d839")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7925573d3f51d1ed9f697e08031031fd2f47be7")]
 [assembly: System.Reflection.AssemblyProductAttribute("01正则表达式")]
 [assembly: System.Reflection.AssemblyTitleAttribute("01正则表达式")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
